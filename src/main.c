01c04f6c4de8c4d4f3a6d86c8f665ffe4efbd03f
raphico <raphicogit@gmail.com>
2025-08-05 01:41:00 +0100

    feat: extract url path

--- src/main.c
+++ src/main.c
@@ -1,9 +1,32 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <sys/socket.h>
+
+typedef struct {
+    char *method;
+    char *url;
+} request;
+
+enum HttpStatus {
+    STATUS_OK = 200,
+    STATUS_NOT_FOUND = 404,
+};
+
+enum ParseError {
+    PARSE_ERR_READ = -1,
+    PARSE_ERR_CLOSED = -2,
+    PARSE_ERR_INVALID = -3
+};
+
+int parse_request(int fd, request *req);
+int send_response(int fd, int status_code);
+const char *status_text(int status_code);
+void cleanup(request *req);
 
 int main() {
     // Disables output buffering
@@ -16,7 +39,7 @@ int main() {
     // opens a TCP socket
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
-        printf("Socket creation failed: %s...\n", strerror(errno));
+        printf("Socket creation failed: %s\n", strerror(errno));
         return 1;
     }
 
@@ -24,7 +47,7 @@ int main() {
     // ensures we don't run into "Already in use" error
     int reuse = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-        printf("SO_REUSEADDR failed: %s...\n", strerror(errno));
+        printf("SO_REUSEADDR failed: %s\n", strerror(errno));
         return 1;
     }
 
@@ -37,7 +60,7 @@ int main() {
 
     // binds the socket to IP + port
     if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
-        printf("bind failed: %s...\n", strerror(errno));
+        printf("bind failed: %s\n", strerror(errno));
         return 1;
     }
 
@@ -46,7 +69,7 @@ int main() {
 
     // marks the open socket as passive, meaning it is ready to receive incoming client connections
     if (listen(server_fd, connection_backlog) != 0) {
-        printf("listen failed: %s...\n", strerror(errno));
+        printf("listen failed: %s\n", strerror(errno));
         return 1;
     }
 
@@ -56,28 +79,131 @@ int main() {
     // blocks and wait until a client connect
     int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
     if (client_fd < 0) {
-        printf("accept failed: %s...\n", strerror(errno));
+        printf("accept failed: %s\n", strerror(errno));
         return 1;
     }
 
 	printf("Client connected\n");
 
-	char *response = "HTTP/1.1 200 OK\r\n\r\n";
-	ssize_t total_sent = 0;
-	ssize_t response_len = strlen(response);
+    request req;
+    int result = parse_request(client_fd, &req);
+    if (result == PARSE_ERR_READ) {
+        printf("failed to read client connection");
+        return -1;
+    }
 
-	while (total_sent < response_len) {
-		ssize_t sent = write(client_fd, response + total_sent, response_len - total_sent);
+    if (result == PARSE_ERR_CLOSED) {
+        printf("connection closed\n");
+        return -1;
+    }
+
+    if (result == PARSE_ERR_INVALID) {
+        printf("invalid request");
+        return -1;
+    }
+
+    if (strcmp(req.url, "/") == 0) {
+        if (send_response(client_fd, STATUS_OK) == -1) {
+            printf("write failed");
+            return -1;
+        }
+    } else {
+        if (send_response(client_fd, STATUS_NOT_FOUND) == -1) {
+            printf("write failed");
+            return -1;
+        }
+    }
+
+    cleanup(&req);
+    close(server_fd);
+    close(client_fd);
+    
+    return 0;
+}
+
+int send_response(int fd, int status_code) {
+    char buf[1024];
+    int len = snprintf(
+        buf, 
+        sizeof(buf), 
+        "HTTP/1.1 %d %s\r\n", 
+        status_code, 
+        status_text(status_code)
+    );
+    if (len < 0 || len >= sizeof(buf)) {
+        return -1;
+    }
+
+	ssize_t total_sent = 0;
+	while (total_sent < len) {
+		ssize_t sent = write(fd, buf + total_sent, len - total_sent);
 		if (sent < 0) {
-			printf("write failed: %s...\n", strerror(errno));
-			break;
+            return -1;
 		}
 
 		total_sent += sent;
 	}
 
-    close(server_fd);
-    close(client_fd);
-    
     return 0;
+}
+
+const char *status_text(int status_code) {
+    switch (status_code) {
+        case STATUS_OK: return "OK";
+        case STATUS_NOT_FOUND: return "Not Found";
+        default: return "Unknown status code";
+    }
+}
+
+int parse_request(int fd, request *req) {
+    char status_line[1024];
+    char buf[1];
+    ssize_t n;
+    int i = 0;
+
+    // reads the status line
+    while (true) {
+        n = read(fd, buf, sizeof(buf));
+        if (n < 0) {
+            return PARSE_ERR_READ;
+        }
+
+        if (n == 0) {
+            return PARSE_ERR_CLOSED;
+        }
+
+        if (buf[0] == '\n') {
+            break;
+        }
+
+        if (buf[0] == '\r') {
+            continue;
+        }
+
+        status_line[i++] = buf[0];
+    }
+
+    status_line[i] = '\0';
+
+    char *save_ptr;
+
+    char *method = strtok_r(status_line, " ", &save_ptr);
+    char *url = strtok_r(NULL, " ", &save_ptr);
+
+    if (method == NULL || url == NULL) {
+        return PARSE_ERR_INVALID;
+    }
+
+    req->method = strdup(method);
+    req->url = strdup(url);
+
+    return 0;
+}
+
+void cleanup(request *req) {
+    free(req->method);
+    free(req->url);
+    
+    req->method = NULL;
+    req->url = NULL;
 }
